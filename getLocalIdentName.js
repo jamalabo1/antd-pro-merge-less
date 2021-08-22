75a8849f18e8d62a183fd9d9c8fa4e4d6faa38c9
Jamal Abo Mokh <jamalabomokh490@gmail.com>
2021-08-22 18:32:45 +0300

    - added log

--- getLocalIdentName.js
+++ getLocalIdentName.js
@@ -1,3 +1,9 @@
 const genericNames = require('generic-names');
 
-module.exports = (className, path) => genericNames('[local]___[hash:base64:5]')(className, path);
+module.exports = (className, path) => {
+  const result = genericNames('[local]___[hash:base64:5]')(className, path);
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`[-]: transform: {${className} to {${result}, path: {${path}}`);
+  }
+  return result;
+};
